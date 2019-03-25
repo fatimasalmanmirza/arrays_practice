975f3d3a0393a4024861e4651d50f8e9f8a56dfe
Fatima <fatima@TongsAppleWatch.localdomain>
2019-03-25 12:03:28 -0700

    converted dec to binary

--- HB-questions.py
+++ HB-questions.py
@@ -12,9 +12,28 @@ print(rev_string("abcdefg"))
 print(rev_string(""))
 ##########################################################
 def rev_string(astring):
-
+	"""without recursion"""
 	revered = ""
 	for i in astring:
 		revered = i + revered
 	return revered
-print(rev_string("abcd"))		
+print(rev_string("abcd"))
+########################################################
+
+def dec2bin(num):
+
+	string = ""
+
+	if num == 0:
+		return num
+	while num != 0:
+
+		remainder = num%2
+		string = str(remainder)+ string
+		num = num//2
+		
+	
+		
+	return string
+print(dec2bin(6))
+
