8157fdc81e65224d083271e6786e919f8b3a4dc5
Fatima <fatima@TongsAppleWatch.localdomain>
2019-03-25 11:07:39 -0700

    reversed a string using recursion

---
+++ HB-questions.py
@@ -0,0 +1,11 @@
+#Hackbright questions
+#Given a string, return a new string that is the original #string, reversed. Do this with recursion, not a for loop.
+
+def rev_string(astring):
+	"""Return reverse of string using recursion.
+	You may NOT use the reversed() function!
+	"""
+	if len(astring) < 2:
+		return astring
+	return astring[-1] + rev_string(astring[0:-1])	
+print(rev_string("abcdefg"))
