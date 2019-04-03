7370d1991dca6456929e40b4d078edd953502b92
Fatima <fatima@Muhammads-MacBook-Pro.local>
2019-04-03 14:50:50 -0700

    need combine str with com

--- pinterest-easy.py
+++ pinterest-easy.py
@@ -12,7 +12,52 @@ def longestCommonPrefix(strs):
 			if i[indx] != val:
 				return shortest_str[:indx]
 	return shortest_str			
-print(longestCommonPrefix(["flower","flow","flight"]))
+# print(longestCommonPrefix(["flower","flow","flight"]))
 # print(longestCommonPrefix(["dog","racecar","car"]))		
 ########################################################
+def subdomainVisits(cpdomains):
+	"""We are given a list cpdomains of count-paired
+	domains. We would like a list of count-paired domains,
+	(in the same format as the input, and in any order),
+	that explicitly counts the number of visits to each 
+	subdomain."""
+	domain_dict = {}
+	for domain in cpdomains:
+		count, domain = domain.split()
+		
+		count = int(count)
+		domain = str(domain)
+
+
+		if domain not in domain_dict:
+			domain_dict[domain] = count
+		else:
+			domain_dict[domain] += count	
+		
+		subdomain = domain.split('.')
+		
+		for i in subdomain:
+			if i not in domain_dict:
+				domain_dict[i] = count
+			else:
+				domain_dict[i] += count
+	return domain_dict				 
+
+	
+		# if domain_s not in domain_dict:
+		# 	domain_dict[domain_s] = count
+		# else:
+		# 	domain_dict[domain_s] += count	
+
+
+
+# print(subdomainVisits(["9001 discuss.leetcode.com"]))		
+print(subdomainVisits(["900 google.mail.com", "50 yahoo.com",
+	"1 intel.mail.com", "5 wiki.org"]))
+
+
+
+
+
+
 
