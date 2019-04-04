5c230b38803f29c143f3919e7f5eac08567127cd
Fatima <fatima@Muhammads-MacBook-Pro.local>
2019-04-04 14:20:37 -0700

    getting long word

--- pinterest-easy.py
+++ pinterest-easy.py
@@ -4,14 +4,16 @@ def longestCommonPrefix(strs):
 	If there is no common prefix, return an empty string"""
 	if not strs:
 		return ""
-	shortest_str = min(strs, key = len)
-	# print(len(shortest_str))
-	for indx, val in enumerate(shortest_str):
-		# print(f"{indx}{val}")
-		for i in strs:
-			if i[indx] != val:
-				return shortest_str[:indx]
-	return shortest_str			
+	min_l = min(len(S) for S in strs)
+	
+	for i in range(min_l):
+		s = set()
+		for x in strs:
+			s.add(x[i])
+			if len(s) > 1:
+				min_l = i
+				break
+	return strs[0][:min_l]		
 # print(longestCommonPrefix(["flower","flow","flight"]))
 # print(longestCommonPrefix(["dog","racecar","car"]))		
 ########################################################
@@ -24,37 +26,51 @@ def subdomainVisits(cpdomains):
 	from collections import Counter
 	domain_dict = Counter()
 	result = []
-	
 	for domain in cpdomains:
 		count, domain = domain.split(" ")
-		
 		count = int(count)
 		d = domain.split(".")
-
-
 		for k in range(1, len(d)+1):
-
 			domain_dict[".".join(d[-k:])]+=count
-
-
 	for keys, values in domain_dict.items():
 		result.append(str(values) + " " +str(keys))
-		
-
 	return result			 
-
-	
-		# if domain_s not in domain_dict:
-		# 	domain_dict[domain_s] = count
-		# else:
-		# 	domain_dict[domain_s] += count	
-
-
-
 # print(subdomainVisits(["9001 discuss.leetcode.com"]))		
-print(subdomainVisits(["900 google.mail.com", "50 yahoo.com",
-	"1 intel.mail.com", "5 wiki.org"]))
+# print(subdomainVisits(["900 google.mail.com", "50 yahoo.com",
+# 	"1 intel.mail.com", "5 wiki.org"]))
+#########################################################
+def split_square_digits(n):
+	s = 0
+	while n >= 1:
+		a = n // 10
+		b = n % 10
+		n = a
+		s += b*b
+	return s	
 
+def ishappy(n):
+# 	"""Write an algorithm to determine if a number is "happy"."""
+	seen = set()
+	while n > 1:
+		seen.add(n)
+		n = split_square_digits(n)		
+		if n in seen:
+			return False
+	return True
+#########################################################	
+def longest_word(words):
+
+	words.sort()
+	words_set = set([""])
+	result =  ''
+	for word in words:
+		if word[:-1] in words_set:
+			words_set.add(word)
+			if len(word) > len(result):
+				result = word
+	return result
+print(longest_word(["w","wo","wor","worl", "world"]))
+print(longest_word(["a", "banana", "app", "appl", "ap", "apply", "apple"]))
 
 
 
