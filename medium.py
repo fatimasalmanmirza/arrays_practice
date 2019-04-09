142e281f21d4f7c3ab4bdd58a01f569e36ee6f77
Fatima <fatima@Muhammads-MacBook-Pro.local>
2019-04-09 11:28:21 -0700

    intersection of two arrays

--- medium.py
+++ medium.py
@@ -19,13 +19,7 @@
 
 def remove_duplicates(nums):
 	"""remove dupliactes in place"""
-	# counter = 0
-	# for i in range(len(nums)-1):
-	# 	if nums[i] != nums[i+1]:
-	# 		counter +=1
-
-	# return counter
-			
+		
 	if len(nums) <= 1:
 		return len(nums)
 	i = 1
@@ -34,7 +28,51 @@ def remove_duplicates(nums):
 			nums.pop(i)
 		else:
 			i += 1
-	return len(nums)				
-print(remove_duplicates([0,0,1,1,1,2,2,3,3,4]))
+	return nums		
+# print(remove_duplicates([0,0,1,1,1,2,2,3,3,4]))
+
+def contain_duplicates(nums):
+	x = set(nums)
+	if len(nums) != len(x):
+		return True
+	return False	
+# print(contain_duplicates([1,1,1,3,3,4,3,2,4,2]))
+
+def singleNumber(nums):
+	
+	count = 0
+	nums.sort()
+	current = nums[0]
+	for i in range(len(nums)):
+		if current == nums[i]:
+			count += 1
+		else:
+			if count == 1:
+				return current
+			current = nums[i]
+			count = 1
+	if count == 1:
+		return current
+# print(singleNumber([4,1,2,1,2]))
+
+def intersect(nums1, nums2):
+	result = []
+	map = {}
+	for i in nums1:
+		if i not in map:
+			map[i] = 1
+		else:
+			map[i] += 1	
+		
+	for j in nums2:
+		if j in map and map[j] > 0:
+			result.append(j)
+			map[j] -= 1
+
+	return result
+print(intersect([1,2,2,1],[2,2]))
+print(intersect([4,9,5],[9,4,9,8,4]))	
+
+
 
 
